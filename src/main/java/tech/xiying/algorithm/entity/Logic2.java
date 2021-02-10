915ad0d09f2ed43af10b6b9db0a1ac1deb501273
shanghao <445664371@qq.com>
2021-02-10 11:20:02 +0800

    [chg]添加单调栈相关demo

--- src/main/java/tech/xiying/algorithm/entity/Logic2.java
+++ src/main/java/tech/xiying/algorithm/entity/Logic2.java
@@ -1,12 +1,36 @@
 package tech.xiying.algorithm.entity;
 
+import java.util.Stack;
+
 public class Logic2 {
 
     public static void main(String[] args) {
-
+        int[] arr = new int[]{69,75,71,91,73,81};
+        int[] ints = dailyTemperatures(arr);
+        for (int i=0;i<ints.length;i++){
+            System.out.print(ints[i]+ " ");
+        }
     }
 
-    private static void equation(){
+    /**
+     * 单调栈解决天气问题
+     *
+     * 69 70 80 91 71 68
+     * 后面几天会超过前面的温度
+     *
+     */
+    private static int[] dailyTemperatures(int[] temp){
+        int[] ans = new int[temp.length];
+        Stack<Integer> stack = new Stack<>();
 
+        // 遍历
+        for(int i=0; i < temp.length ;i++){
+            // 若后一天的天气大于栈顶元素，则栈顶元素弹出
+            while(!stack.empty() && temp[i] > temp[stack.peek()]){
+                ans[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return  ans;
     }
 }
